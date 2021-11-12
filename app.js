75119cd1289d4f49e788fb64f41cef7eda099545
Abdillahi Osman <hajiabdi213@gmail.com>
2021-11-12 16:57:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+let display=document.querySelector('h1')
+let result=0;
+display.innerText=result
+
+function count(){
+result++
+  display.innerText= result;
+   
+}
+
+//clear
+function cl(){
+    result=0
+    display.innerText= result;
+}
+
+
